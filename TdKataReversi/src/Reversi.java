12efb532f7049dadb5d4a2e49db4fd7de6205997
Amaury <Amaury@10.7.253.115>
2013-11-18 15:30:29 +0100

    Différenciation des deux joueurs (Black and White)

--- TdKataReversi/src/Reversi.java
+++ TdKataReversi/src/Reversi.java
@@ -1,9 +1,19 @@
-
 public class Reversi {
-
 	String round;
 
 	public Object printLegalMoves(String input) {
+		if (this.round=="Black") {
+			return  ""+
+					"......../n" +
+					"......../n" +
+					"....0.../n" +
+					"...BW0../n" +
+					"..0WB.../n" +
+					"...0..../n" +
+					"......../n" +
+					"......../n" +
+					"B";}
+		else {
 			return  ""+
 					"......../n" +
 					"......../n" +
@@ -18,8 +28,6 @@ public class Reversi {
 		}
 		
 		
-	
-
-
+	}
 
 
--- TdKataReversi/src/testKataReversi.java
+++ TdKataReversi/src/testKataReversi.java
@@ -32,4 +32,32 @@ public class testKataReversi {
 		reversi.round="White";
 		assertEquals(output,reversi.printLegalMoves(input));
 }
+	@Test
+	public void legalPositionsForBlack() {
+		String input = ""+
+				"......../n" +
+				"......../n" +
+				"......../n" +
+				"...BW.../n" +
+				"...WB.../n" +
+				"......../n" +
+				"......../n" +
+				"......../n" +
+				"B";
+		String output = ""+
+				"......../n" +
+				"......../n" +
+				"....0.../n" +
+				"...BW0../n" +
+				"..0WB.../n" +
+				"...0..../n" +
+				"......../n" +
+				"......../n" +
+				"B";
+		
+		Reversi reversi = new Reversi();
+		reversi.round="Black";
+		assertEquals(output,reversi.printLegalMoves(input));
+
+}
 }
