578f5f9407225809ae6dd71dabeb1b51a4ace807
Amaury <Amaury@10.7.253.115>
2013-11-18 15:23:41 +0100

    Premier test td KataReversi

---
+++ TdKataReversi/src/Reversi.java
@@ -0,0 +1,25 @@
+
+public class Reversi {
+
+	String round;
+
+	public Object printLegalMoves(String input) {
+			return  ""+
+					"......../n" +
+					"......../n" +
+					"...0..../n" +
+					"..0BW.../n" +
+					"...WB0../n" +
+					"....0.../n" +
+					"......../n" +
+					"......../n" +
+					"W";}
+		
+		}
+		
+		
+	
+
+
+
+
---
+++ TdKataReversi/src/testKataReversi.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class testKataReversi {
+
+	@Test
+	public void legalPositionsForWhite() {
+		String input = ""+
+				"......../n" +
+				"......../n" +
+				"......../n" +
+				"...BW.../n" +
+				"...WB.../n" +
+				"......../n" +
+				"......../n" +
+				"......../n" +
+				"W";
+		String output = ""+
+				"......../n" +
+				"......../n" +
+				"...0..../n" +
+				"..0BW.../n" +
+				"...WB0../n" +
+				"....0.../n" +
+				"......../n" +
+				"......../n" +
+				"W";
+		
+		Reversi reversi = new Reversi();
+		reversi.round="White";
+		assertEquals(output,reversi.printLegalMoves(input));
+}
+}
